fd28837dcd54784aa824c73e5115faa5fb90555d
Alex De <segoy@interia.pl>
2023-11-29 00:33:32 +0100

    edit Main.class

--- src/main/java/com/alex/passwordgenerator/Main.java
+++ src/main/java/com/alex/passwordgenerator/Main.java
@@ -1,5 +1,6 @@
 package com.alex.passwordgenerator;
 
+// class created for fat jar purposes (it doesn't like PassGeneratorApplication extending Application)
 public class Main {
     public static void main(String[] args) {
         PassGeneratorApplication.main(args);
